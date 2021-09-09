9a0c01545477949f7f352398d3c08290bfefa944
Your Name <csyedabdulrafeyali@gmail.com>
2021-09-09 23:47:42 +0500

    main__webAPI

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Data;
+using MusicAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private ApiDbContext _dbContext;
+
+        public SongsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return _dbContext.Songs;
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public Song Get(int id)
+        {
+            return _dbContext.Songs.Find(id);
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public void Post([FromBody] Song song)
+        {
+           _dbContext.Songs.Add(song);
+           _dbContext.SaveChanges();
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Song song)
+        {
+            var index = _dbContext.Songs.Find(id);
+            index.Title = song.Title;
+            index.Language = song.Language;
+            _dbContext.SaveChanges();
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var song = _dbContext.Songs.Find(id);
+            _dbContext.Songs.Remove(song);
+            _dbContext.SaveChanges();
+        }
+    }
+}
